59b84e9e747e83863855505b9c9b056374a54b25
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2016-10-21 13:49:41 +0200

    Move existing files into app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+function refreshData() {
+  var d = new Date();
+
+  var h = ('0' + d.getHours()).slice(-2);
+  var m = ('0' + d.getMinutes()).slice(-2);
+  var s = ('0' + d.getSeconds()).slice(-2);
+
+  var color = h + m + s;
+
+  document.getElementById('background').style.backgroundColor = '#' + color;
+  document.getElementById('clock').textContent = color;
+}
+
+window.setInterval(refreshData, 1000);
+refreshData(); // execute function
