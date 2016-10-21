716f6d12e9a95451365d3d7bdbcb8e2832175616
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2016-10-21 15:15:14 +0200

    ES2015-ify main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,14 @@
 function refreshData() {
-  var d = new Date();
+  const d = new Date();
 
-  var h = ('0' + d.getHours()).slice(-2);
-  var m = ('0' + d.getMinutes()).slice(-2);
-  var s = ('0' + d.getSeconds()).slice(-2);
+  const
+    h = `0${d.getHours()}`.slice(-2),
+    m = `0${d.getMinutes()}`.slice(-2),
+    s = `0${d.getSeconds()}`.slice(-2);
 
-  var color = h + m + s;
+  const color = h + m + s;
 
-  document.getElementById('background').style.backgroundColor = '#' + color;
+  document.getElementById('background').style.backgroundColor = `#${color}`;
   document.getElementById('clock').textContent = color;
 }
 
