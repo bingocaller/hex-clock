dde8ccca259af99c19aad984f0062444c27906e5
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2018-03-13 15:46:18 +0100

    Fixes for styles, JS, markup
    JS: fix RGB math, use function syntax, getElementById -> querySelector
    SCSS: A bit of linting, use classess over ids
    HTML: Update script reference, title, name, description

--- app/scripts/colour-adjusted.js
+++ app/scripts/colour-adjusted.js
@@ -1,32 +1,33 @@
 // Get RGB channels from a date
-const RGBFromDate = (date) => ([
-    (255 / (date.getHours() * 24)),
-    (255 / (date.getMinutes() * 60)),
-    (255 / (date.getSeconds() * 60))
+function RGBFromDate(date) {
+  return [
+    255 - (date.getHours() / 24 * 255),
+    255 - (date.getMinutes() / 60 * 255),
+    255 - (date.getSeconds() / 60 * 255),
   ].map(Math.round)
-);
+}
 
 // Get color luminance as a float from RGB channels
-const colorLuminance = (red, green, blue) => (
-  ((0.299 * red) + (0.587 * green) + (0.114 * blue)) / 256
-);
+function colorLuminance(red, green, blue) {
+  return ((0.299 * red) + (0.587 * green) + (0.114 * blue)) / 256;
+}
 
 // Get font color from RGB channels from background
-const colorFromRGB = (red, green, blue) => (
-  colorLuminance(red, green, blue) > 0.7 ? 'rgba(0,0,0, .8)' : 'rgba(255,255,255, .8)'
-);
+function colorFromRGB(red, green, blue) {
+  return colorLuminance(red, green, blue) > 0.7 ? 'rgba(0,0,0, .8)' : 'rgba(255,255,255, .8)';
+}
 
 // Get formatted date
-const dateToContent = (date) => (
-  date.toTimeString().replace(/.*(\d{2}):(\d{2}):(\d{2}).*/, '$1$2$3')
-);
+function dateToContent(date) {
+  return date.toTimeString().replace(/.*(\d{2}):(\d{2}):(\d{2}).*/, '$1$2$3');
+}
 
-const colorClock = () => {
+function colorClock() {
   const date = new Date();
   const channels = RGBFromDate(date);
 
-  const clock = document.getElementById('clock');
-  const bg = document.getElementById('background');
+  const clock = document.querySelector('.clock');
+  const bg = document.querySelector('.background');
 
   bg.style.backgroundColor = `rgb(${channels.join(',')})`;
   clock.style.color = colorFromRGB.apply(this, channels);
